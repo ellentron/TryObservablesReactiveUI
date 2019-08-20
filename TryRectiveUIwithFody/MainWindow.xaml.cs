7b6a811d36cdfd3bf51a5c58c633b90a81aa5735
Danny Ellenbogen <danny@ellentron.com>
2019-08-20 16:59:33 +0300

    Working the same as the previous project

---
+++ TryRectiveUIwithFody/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using ReactiveUI;
+using System.Reactive.Disposables;
+using System.Windows;
+
+namespace TryRectiveUIwithFody
+{
+    // MainWindow class derives off ReactiveWindow which implements the IViewFor<TViewModel>
+    // interface using a WPF DependencyProperty. We need this to use WhenActivated extension
+    // method that helps us handling View and ViewModel activation and deactivation.
+    public partial class MainWindow : ReactiveWindow<MyViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ViewModel = new MyViewModel();
+
+            // We create our bindings here. These are the code behind bindings which allow 
+            // type safety. The bindings will only become active when the Window is being shown.
+            // We register our subscription in our disposableRegistration, this will cause 
+            // the binding subscription to become inactive when the Window is closed.
+            // The disposableRegistration is a CompositeDisposable which is a container of 
+            // other Disposables. We use the DisposeWith() extension method which simply adds 
+            // the subscription disposable to the CompositeDisposable.
+            this.WhenActivated(disposable =>
+            {
+                this.Bind(ViewModel, x => x.TheValue, x => x.TheValueBox.Text)
+                    .DisposeWith(disposable);
+
+                this.OneWayBind(ViewModel, x => x.TheValue, x => x.TheTextBlock.Text)
+                    .DisposeWith(disposable);
+
+                this.BindCommand(ViewModel, x => x.SetValueTo1000000Command, x => x.TheTextButton)
+                    .DisposeWith(disposable);
+
+                this.Bind(ViewModel, x => x.TheName, x => x.NameTextBox.Text)
+                    .DisposeWith(disposable);
+
+                this.OneWayBind(ViewModel, x => x.FirstName, x => x.NameTextBlock.Text)
+                    .DisposeWith(disposable);
+
+
+                //this.OneWayBind(ViewModel,
+                //   viewModel => viewModel.Name,
+                //   view => view.Name.Text);
+
+                //this.Bind(ViewModel,
+                //    viewModel => viewModel.SearchTerm,
+                //    view => view.searchTextBox.Text)
+                //    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
---
+++ TryRectiveUIwithFody/MyViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace TryRectiveUIwithFody
+{
+    public class MyViewModel : ReactiveObject
+    {
+
+        #region Constructor
+        public MyViewModel()
+        {
+            this.WhenAnyValue(x => x.TheName)
+                .Select(theName => theName?.Split(' ')[0])
+                .ToPropertyEx(this, x => x.FirstName);
+
+            SetValueTo1000000Command = ReactiveCommand.CreateFromObservable(ExecuteSetValueTo1000000Command);
+
+        }
+        #endregion
+
+        #region Properties
+        [Reactive] public string TheName { get; set; }
+
+        [Reactive] public double TheValue { get; set; }
+
+        public extern string FirstName { [ObservableAsProperty]get; }
+
+        /// <summary>
+        /// SetValueTo1000000Command (a ReactiveUI command)
+        /// </summary>
+        public ReactiveCommand<Unit, Unit> SetValueTo1000000Command { get; set; }
+
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Command that executes when 
+        /// </summary>
+        /// <returns></returns>
+        private IObservable<Unit> ExecuteSetValueTo1000000Command()
+        {
+            //TheValue = "Hello ReactiveUI";
+            TheValue = 10000000;
+            return Observable.Return(Unit.Default);
+        }
+        #endregion
+
+
+    }
+}
